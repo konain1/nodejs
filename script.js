fee133e5e6665a2590e0506f41ab35a6492ef885
Konain1 <kaunainkasab@gmail.com>
2023-11-03 15:50:56 +0530

    middleware

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const express = require('express')
 const app = express();
 let port  = 3001
 
+app.use((req,res,next)=>{
+    console.log('middle ware')
+    next()
+})
 app.get('/' , (req,res)=>{
     res.send('http method')
 })
